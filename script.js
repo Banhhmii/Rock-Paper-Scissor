364286a725863980bc17148790c5994157dff06d
Tommy <tommyngo156@gmail.com>
2026-02-10 22:58:27 -0500

    Wrote and test getComputerChoice()

---
+++ script.js
@@ -0,0 +1,12 @@
+let getComputerChoice = () => {
+    let randomNum = Math.floor(Math.random() * 3);
+    switch (randomNum) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+    }
+}
+
